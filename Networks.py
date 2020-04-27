5fe9859f319c767e95706f61cfe0f2ea13bed57b
Ethan Pappion <ethanpappion@pop-os.localdomain>
2020-04-26 23:55:33 -0500

    finished 1 layer network, added total results to console

---
+++ Networks.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+class Perceptron():
+    def __init__(self):
+        # Seed the random number generator
+        np.random.seed(1)
+
+        #Input layer, 3x1
+        self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
+
+    def sigmoid(self, x):
+        """
+        Takes in weighted sum of the inputs and normalizes
+        them through between 0 and 1 through a sigmoid function
+        """
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        """
+        The derivative of the sigmoid function used to
+        calculate necessary weight adjustments
+        """
+        return x * (1 - x)
+
+    def train(self, training_inputs, training_outputs, training_iterations):
+        """
+        We train the model through trial and error, adjusting the
+        synaptic weights each time to get a better result
+        """
+        for iteration in range(training_iterations):
+            # Pass training set through the neural network
+            output = self.think(training_inputs)
+
+            # Calculate the error
+            error = (training_outputs - output)
+
+            # Multiply error by input and gradient of the sigmoid function
+            # Less confident weights are adjusted more through the nature of the function
+            adjustments = np.dot(training_inputs.T, error * self.sigmoid_derivative(output))
+
+            # Adjust synaptic weights
+            self.synaptic_weights += adjustments
+
+    def think(self, inputs):
+        """
+        Pass inputs through the neural network to get output
+        """
+
+        inputs = inputs.astype(float)
+        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
+        return output
+
+class NeuralNetwork():
+    def __init__(self):
+        np.random.seed(7) # for generating the same results
+        self.wij   = np.random.rand(3,4) # input to hidden layer weights
+        self.wjk   = np.random.rand(4,1) # hidden layer to output weights
+
+    def sigmoid(self, x, w):
+        z = np.dot(x, w)
+        return 1/(1 + np.exp(-z))
+
+    def sigmoid_derivative(self, x, w):
+        return self.sigmoid(x, w) * (1 - self.sigmoid(x, w))
+
+    def train(self, x, y, iterations):
+        for i in range(iterations):
+            Xi = x
+            Xj = self.sigmoid(Xi, self.wij)
+            yhat = self.sigmoid(Xj, self.wjk)
+            # gradients for hidden to output weights
+            g_wjk = np.dot(Xj.T, (y - yhat) * self.sigmoid_derivative(Xj, self.wjk))
+            # gradients for input to hidden weights
+            g_wij = np.dot(Xi.T, np.dot((y - yhat) * self.sigmoid_derivative(Xj, self.wjk), self.wjk.T) * self.sigmoid_derivative(Xi, self.wij))
+            # update weights
+            self.wij += g_wij
+            self.wjk += g_wjk
+
+    def think(self, input):
+        input = input.astype(float)
+        Xj = self.sigmoid(input, self.wij)
+        output = self.sigmoid(Xj, self.wjk)
+        return output
--- main.py
+++ main.py
@@ -1,95 +1,21 @@
 import numpy as np
-
-class Perceptron():
-    def __init__(self):
-        # Seed the random number generator
-        np.random.seed(1)
-
-        #Input layer, 3x1
-        self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
-
-    def sigmoid(self, x):
-        """
-        Takes in weighted sum of the inputs and normalizes
-        them through between 0 and 1 through a sigmoid function
-        """
-        return 1 / (1 + np.exp(-x))
-
-    def sigmoid_derivative(self, x):
-        """
-        The derivative of the sigmoid function used to
-        calculate necessary weight adjustments
-        """
-        return x * (1 - x)
-
-    def train(self, training_inputs, training_outputs, training_iterations):
-        """
-        We train the model through trial and error, adjusting the
-        synaptic weights each time to get a better result
-        """
-        for iteration in range(training_iterations):
-            # Pass training set through the neural network
-            output = self.think(training_inputs)
-
-            # Calculate the error
-            error = (training_outputs - output)
-
-            # Multiply error by input and gradient of the sigmoid function
-            # Less confident weights are adjusted more through the nature of the function
-            adjustments = np.dot(training_inputs.T, error * self.sigmoid_derivative(output))
-
-            # Adjust synaptic weights
-            self.synaptic_weights += adjustments
-
-            print(self.synaptic_weights)
-
-    def think(self, inputs):
-        """
-        Pass inputs through the neural network to get output
-        """
-
-        inputs = inputs.astype(float)
-        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
-        return output
-
-class NeuralNetwork():
-    def __init__(self):
-        np.random.seed(10) # for generating the same results
-        self.wij   = np.random.rand(3,4) # input to hidden layer weights
-        self.wjk   = np.random.rand(4,1) # hidden layer to output weights
-
-    def sigmoid(self, x, w):
-        z = np.dot(x, w)
-        return 1/(1 + np.exp(-z))
-
-    def sigmoid_derivative(self, x, w):
-        return self.sigmoid(x, w) * (1 - self.sigmoid(x, w))
-
-    def gradient_descent(self, x, y, iterations):
-        for i in range(iterations):
-            Xi = x
-            Xj = self.sigmoid(Xi, self.wij)
-            yhat = self.sigmoid(Xj, self.wjk)
-            # gradients for hidden to output weights
-            g_wjk = np.dot(Xj.T, (y - yhat) * self.sigmoid_derivative(Xj, self.wjk))
-            # gradients for input to hidden weights
-            g_wij = np.dot(Xi.T, np.dot((y - yhat) * self.sigmoid_derivative(Xj, self.wjk), self.wjk.T) * self.sigmoid_derivative(Xi, self.wij))
-            # update weights
-            self.wij += g_wij
-            self.wjk += g_wjk
-        print('The final prediction from neural network are: ')
-        print(yhat)
+import Networks
 
 if __name__ == "__main__":
 
     # Initialize the single neuron neural network
-    perceptron_network = Perceptron()
+    perceptron_network = Networks.Perceptron()
+    neural_network = Networks.NeuralNetwork()
 
-    print("Random starting synaptic weights: ")
+    print("Perceptron weights: ")
     print(perceptron_network.synaptic_weights)
 
-    # The training set, with 4 examples consisting of 3
-    # input values and 1 output value
+    print("Neural network (1 layer) input weights: ")
+    print(neural_network.wij)
+    print("Neural network (1 layer) hidden layer weights: ")
+    print(neural_network.wjk)
+
+
     training_inputs = np.array([[0,0,1],
                                 [1,1,1],
                                 [1,0,1],
@@ -97,29 +23,21 @@ if __name__ == "__main__":
 
     training_outputs = np.array([[0,1,1,0]]).T
 
-    # Train the neural network
-    perceptron_network.train(training_inputs, training_outputs, 10000)
+    # Train
+    iterations = int(input("Train networks how many times? "))
+    perceptron_network.train(training_inputs, training_outputs, iterations)
+    neural_network.train(training_inputs, training_outputs, 40000)
 
-    print("Synaptic weights after training: ")
-    print(perceptron_network.synaptic_weights)
-
+    #Give new input
+    print("new inputs to test: ")
     A = str(input("Input 1: "))
     B = str(input("Input 2: "))
     C = str(input("Input 3: "))
 
-    print("New situation: input data = ", A, B, C)
-    print("Perceptron output: ")
-    print(perceptron_network.think(np.array([A, B, C])))
+    userInput = np.array([A, B, C])
 
-    #initializing neural network with 1 hidden layer
-    neural_network1 = NeuralNetwork()
-
-    #print weights for network
-    print("NN weights for input layer:")
-    print(neural_network1.wij)
-
-    print("NN weights for hidden layer:")
-    print(neural_network1.wjk)
-
-    #train
-    
+    #display output
+    print("Perceptron prediction: ")
+    print(perceptron_network.think(userInput))
+    print("Neural network (1 layer) prediction: ")
+    print(neural_network.think(userInput))
